9be007f67aa132744e00244802f11158bf2ba744
Craig Knott <pestilencexp@googlemail.com>
2013-07-04 03:08:03 +0100

    getCurrentSelection returns in a friendly format

--- src/com/craigknott/setLocker/CommandManager.java
+++ src/com/craigknott/setLocker/CommandManager.java
@@ -13,52 +13,67 @@ import com.sk89q.worldedit.bukkit.selections.Selection;
 public class CommandManager implements CommandExecutor {
 
 	private SetLocker plugin;
-	
-	public CommandManager(SetLocker plugin){
-			this.plugin = plugin;
+
+	public CommandManager(SetLocker plugin) {
+		this.plugin = plugin;
 	}
 
-	public void sendError(CommandSender sender, String message){
+	public void sendError(CommandSender sender, String message) {
 		sender.sendMessage(ChatColor.valueOf("RED").toString().concat(message));
 	}
-	
-	public boolean getCurrentSelection(CommandSender sender, String label, String[] args) {
+
+	public boolean getCurrentSelection(CommandSender sender, String label,
+			String[] args) {
 		if (!(sender instanceof Player)) {
 			sendError(sender, "This command may only be invoked by a player");
 			return true;
-		} 
-		
-		if ( args.length > 0 ){
-			sendError(sender, "Incorrect number of arguments given (expected 0, given " + args.length + ")");
+		}
+
+		if (args.length > 0) {
+			sendError(sender,
+					"Incorrect number of arguments given (expected 0, given "
+							+ args.length + ")");
 			return true;
 		}
-			
-		WorldEditPlugin worldEdit = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+
+		WorldEditPlugin worldEdit = (WorldEditPlugin) Bukkit.getServer()
+				.getPluginManager().getPlugin("WorldEdit");
 		Selection selection = worldEdit.getSelection((Player) sender);
-		sender.sendMessage("Max: " + selection.getMaximumPoint() + ", Min: " + selection.getMinimumPoint());
+
+		if (selection != null) {
+			sender.sendMessage("X1 " + selection.getMaximumPoint().getBlockX()
+					+ " Y1 " + selection.getMaximumPoint().getBlockY() + " Z1 "
+					+ selection.getMaximumPoint().getBlockZ() + "\n" + "X2 "
+					+ selection.getMinimumPoint().getBlockX() + " Y2 "
+					+ selection.getMinimumPoint().getBlockY() + " Z2 "
+					+ selection.getMinimumPoint().getBlockZ());
+		} else {
+			sendError(sender, "No selection has been made");
+		}
 		return true;
 	}
-	
-	public boolean cset(CommandSender sender, String label, String[] args){
+
+	public boolean cset(CommandSender sender, String label, String[] args) {
 		StringBuilder sb = new StringBuilder();
-		for ( int i = 0 ; i < args.length ; i ++){
-			sb.append(args[i] +  " ");
+		for (int i = 0; i < args.length; i++) {
+			sb.append(args[i] + " ");
 		}
 		sender.sendMessage(sb.toString());
 		return true;
 	}
-	
+
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
-		if (cmd.getName().equalsIgnoreCase("getCurrentSelection")){
-			return getCurrentSelection(sender,label,args);
-		} 
-		
-		if (cmd.getName().equalsIgnoreCase("cset")){
-			return cset(sender,label,args);
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+
+		if (cmd.getName().equalsIgnoreCase("getCurrentSelection")) {
+			return getCurrentSelection(sender, label, args);
+		}
+
+		if (cmd.getName().equalsIgnoreCase("setlock")) {
+			return cset(sender, label, args);
 		}
 		return false;
 	}
-	
+
 }
