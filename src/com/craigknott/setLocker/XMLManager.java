f77a281d0d2198f15dd90a70a2fe535393300706
Craig Knott <pestilencexp@googlemail.com>
2013-07-04 22:25:55 +0100

    Warden/Cellmates now save

--- src/com/craigknott/setLocker/XMLManager.java
+++ src/com/craigknott/setLocker/XMLManager.java
@@ -18,8 +18,50 @@ public class XMLManager {
 					"setlockersaves.xml"));
 			LockManager lm = c.getLockManager();
 			for ( Lock l : lm.getLocks() ){
-				out.write(l.getRegion().getName());
+				out.write("<lock>\n");
+				out.write("<warden>\n");
+				if ( l.isLocked() ){
+					out.write(l.getWarden() + "\n");
+				} else {
+					out.write("N/A\n");
+				}
+				out.write(l.getWarden() + "\n");
+				out.write("</warden>\n");
+				out.write("<cellmates>\n");
+				for ( String s : l.getCellMates() ){
+					out.write("<cellmate>\n");
+					out.write(s + "\n");
+					out.write("</cellmate>\n");
+				}
+				out.write("</cellmates>\n");
+				out.write("<region>\n");
+				out.write("<name>\n");
+				out.write(l.getRegion().getName() +"\n");
+				out.write("</name>\n");
+				out.write("<min_point>\n");
+				out.write("<world>\n");
+				out.write("</world>\n");
+				out.write("<x>\n");
+				out.write("</x>\n");
+				out.write("<y>\n");
+				out.write("</y>\n");
+				out.write("<z>\n");
+				out.write("</z>\n");
+				out.write("</min_point>\n");
+				out.write("<max_point>\n");
+				out.write("<world>\n");
+				out.write("</world>\n");
+				out.write("<x>\n");
+				out.write("</x>\n");
+				out.write("<y>\n");
+				out.write("</y>\n");
+				out.write("<z>\n");
+				out.write("</z>\n");
+				out.write("</max_point>\n");
+				out.write("</region>\n");
+				out.write("</lock>\n");
 			}
+			
 			out.close();			
 		} catch (IOException e) {
 			System.err.println("[SetLocker] Error saving to setlockersaves.xml");
