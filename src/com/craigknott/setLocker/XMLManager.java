dd908329b707be6e5597455a1ade0c6073000e52
Craig Knott <pestilencexp@googlemail.com>
2013-07-04 22:44:59 +0100

    Output formatting

--- src/com/craigknott/setLocker/XMLManager.java
+++ src/com/craigknott/setLocker/XMLManager.java
@@ -19,37 +19,31 @@ public class XMLManager {
 			LockManager lm = c.getLockManager();
 			for ( Lock l : lm.getLocks() ){
 				out.write("<lock>\n");
-				out.write("<warden>\n");
 				if ( l.isLocked() ){
-					out.write(l.getWarden() + "\n");
+					out.write(" <warden>" + l.getWarden() + "</warden>\n");
 				} else {
-					out.write("N/A\n");
+					out.write(" <warden>N/A</warden>\n");
 				}
-				out.write("</warden>\n");
-				out.write("<cellmates>\n");
+				out.write(" <cellmates>\n");
 				for ( String s : l.getCellMates() ){
-					out.write("<cellmate>\n");
-					out.write(s + "\n");
-					out.write("</cellmate>\n");
+					out.write("  <cellmate>" + s + "</cellmate>");
 				}
-				out.write("</cellmates>\n");
-				out.write("<region>\n");
-				out.write("<name>\n");
-				out.write(l.getRegion().getName() +"\n");
-				out.write("</name>\n");
-				out.write("<min_point>\n");
-				out.write("<world>" + l.getRegion().getMin_point().getWorld() + "</world>\n");
-				out.write("<x>"+ l.getRegion().getMin_point().getX() +"</x>\n");
-				out.write("<y>"+ l.getRegion().getMin_point().getY() +"</y>\n");
-				out.write("<z>"+ l.getRegion().getMin_point().getZ() +"</z>\n");
-				out.write("</min_point>\n");
-				out.write("<max_point>\n");
-				out.write("<world>" + l.getRegion().getMax_point().getWorld() + "</world>\n");
-				out.write("<x>"+ l.getRegion().getMax_point().getX() +"</x>\n");
-				out.write("<y>"+ l.getRegion().getMax_point().getY() +"</y>\n");
-				out.write("<z>"+ l.getRegion().getMax_point().getZ() +"</z>\n");
-				out.write("</max_point>\n");
-				out.write("</region>\n");
+				out.write(" </cellmates>\n");
+				out.write(" <region>\n");
+				out.write("  <name>" + l.getRegion().getName() + "</name>\n");
+				out.write("  <min_point>\n");
+				out.write("   <world>" + l.getRegion().getMin_point().getWorld() + "</world>\n");
+				out.write("   <x>"+ l.getRegion().getMin_point().getX() +"</x>\n");
+				out.write("   <y>"+ l.getRegion().getMin_point().getY() +"</y>\n");
+				out.write("   <z>"+ l.getRegion().getMin_point().getZ() +"</z>\n");
+				out.write("  </min_point>\n");
+				out.write("  <max_point>\n");
+				out.write("   <world>" + l.getRegion().getMax_point().getWorld() + "</world>\n");
+				out.write("   <x>"+ l.getRegion().getMax_point().getX() +"</x>\n");
+				out.write("   <y>"+ l.getRegion().getMax_point().getY() +"</y>\n");
+				out.write("   <z>"+ l.getRegion().getMax_point().getZ() +"</z>\n");
+				out.write("  </max_point>\n");
+				out.write(" </region>\n");
 				out.write("</lock>\n");
 			}
 			
